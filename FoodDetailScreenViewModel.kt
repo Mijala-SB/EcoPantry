28c02f12483173500ba416f6feffbe734bc51f5c
MSB <163714424+Mijala-SB@users.noreply.github.com>
2026-07-10 07:51:56 +0545

    Delete FoodDetailScreenViewModel.kt

--- FoodDetailScreenViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.ecopantry.app.model
-
-/** Status of a donation listing stored in Firebase Firestore (FR07-FR09, US 5.1-5.3). */
-enum class DonationStatus {
-    AVAILABLE,
-    CLAIMED
-}
