33d07bd307d28324bae55878ea31e54b2aebe2c7
Sailabee Shrestha <167024312+Beeinsky@users.noreply.github.com>
2026-07-24 11:54:56 +0545

    StorageArea.kt
    StorageArea.kt UC3

---
+++ Application/app/src/main/java/com/mishba/ecopantryapp/model/StorageArea.kt
@@ -0,0 +1,10 @@
+package com.mishba.ecopantryapp.model
+
+/** Where a food item is physically stored in the household (FR05). */
+enum class StorageArea(val label: String) {
+    FRIDGE("Fridge"),
+    FREEZER("Freezer"),
+    PANTRY("Pantry"),
+    COUNTERTOP("Countertop"),
+    OTHER("Other")
+}
