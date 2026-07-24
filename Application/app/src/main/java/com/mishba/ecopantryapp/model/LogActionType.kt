29af569dbef2cdd51a0c3c56976e4fa8a77f943e
Sailabee Shrestha <167024312+Beeinsky@users.noreply.github.com>
2026-07-24 11:52:28 +0545

    LogActionType.kt
    LogActionType.kt UC3

---
+++ Application/app/src/main/java/com/mishba/ecopantryapp/model/LogActionType.kt
@@ -0,0 +1,10 @@
+package com.mishba.ecopantryapp.model
+
+/** Action recorded to food_log every time inventory changes (US 3.2, 6.1). */
+enum class LogActionType(val label: String) {
+    ADDED("Added"),
+    EDITED("Edited"),
+    USED("Used"),
+    DONATED("Donated"),
+    DELETED("Deleted")
+}
