00098509f939a50152c2c78392b0e7227585b496
Sailabee Shrestha <167024312+Beeinsky@users.noreply.github.com>
2026-07-24 11:54:01 +0545

    NotificationType.kt
    NotificationType.kt

---
+++ Application/app/src/main/java/com/mishba/ecopantryapp/model/NotificationType.kt
@@ -0,0 +1,8 @@
+package com.mishba.ecopantryapp.model
+
+/** Distinguishes the alerts shown on the Notifications screen (FR11, FR12). */
+enum class NotificationType {
+    EXPIRY_ALERT,
+    DONATION_CLAIMED,
+    DONATION_CONFIRMED
+}
